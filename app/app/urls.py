91d404f5bd657854a0cafe7d40beb50df9a641a8
bertholdt <122198422+thethien2906@users.noreply.github.com>
2025-04-22 12:33:27 +0700

    user app create

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,6 +24,6 @@ urlpatterns = [
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs',
-    )
-
+    ),
+    path('api/user/', include('user.urls')),
 ]
---
+++ app/user/serializers.py
@@ -0,0 +1,18 @@
+"""
+Serializers for the user API views.
+"""
+from django.contrib.auth import get_user_model
+
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer for the user object."""
+
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
+
+    def create(self, validated_data):
+        """Create a new user with encrypted password and return it."""
+        return get_user_model().objects.create_user(**validated_data)
