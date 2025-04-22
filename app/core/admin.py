3b1297abc536c1f1d2cad849b817712afaf235fc
bertholdt <122198422+thethien2906@users.noreply.github.com>
2025-04-22 10:30:36 +0700

    drf_spectacular implementation with swagger

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,28 @@
-from django.contrib import admin  # noqa
+"""
+Django admin customization.
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from core import models
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    list_filter = ['is_active', 'is_staff']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff')}),
+        ('Personal Info', {'fields': ('name',)}),
+        ('Important Dates', {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'name', 'is_active', 'is_staff'),
+        }),
+    )
+
+admin.site.register(models.User, UserAdmin)
